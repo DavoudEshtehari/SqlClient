3866613bac43b1defb25eb850288ed261e77779c
Davoud Eshtehari <v-deshtehari@microsoft.com>
2022-05-27 13:28:39 -0700

    fix

--- src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/Common/AdapterUtil.SqlClient.cs
+++ src/Microsoft.Data.SqlClient/netcore/src/Microsoft/Data/Common/AdapterUtil.SqlClient.cs
@@ -11,6 +11,8 @@ using System.Globalization;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Transactions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
 
 namespace Microsoft.Data.Common
 {
@@ -47,9 +49,10 @@ namespace Microsoft.Data.Common
             IndexOutOfRangeException e = new IndexOutOfRangeException();
             return e;
         }
-        internal static TimeoutException TimeoutException(string error)
+        static internal TimeoutException TimeoutException(string error, Exception inner = null)
         {
-            TimeoutException e = new TimeoutException(error);
+            TimeoutException e = new TimeoutException(error, inner);
+            TraceExceptionAsReturnValue(e);
             return e;
         }
         internal static InvalidOperationException InvalidOperation(string error, Exception inner)
@@ -901,6 +904,32 @@ namespace Microsoft.Data.Common
         internal static ArgumentException MustBeReadOnly(string argumentName)
         {
             return Argument(System.StringsHelper.GetString(Strings.ADP_MustBeReadOnly, argumentName));
+        }
+
+        internal static Exception CreateSqlException(MsalException msalException, SqlConnectionString connectionOptions, SqlInternalConnectionTds sender, string username)
+        {
+            // Error[0]
+            SqlErrorCollection sqlErs = new();
+
+            sqlErs.Add(new SqlError(0, (byte)0x00, (byte)TdsEnums.MIN_ERROR_CLASS,
+                                    connectionOptions.DataSource,
+                                    StringsHelper.GetString(Strings.SQL_MSALFailure, username, connectionOptions.Authentication.ToString("G")),
+                                    ActiveDirectoryAuthentication.MSALGetAccessTokenFunctionName, 0));
+
+            // Error[1]
+            string errorMessage1 = StringsHelper.GetString(Strings.SQL_MSALInnerException, msalException.ErrorCode);
+            sqlErs.Add(new SqlError(0, (byte)0x00, (byte)TdsEnums.MIN_ERROR_CLASS,
+                                    connectionOptions.DataSource, errorMessage1,
+                                    ActiveDirectoryAuthentication.MSALGetAccessTokenFunctionName, 0));
+
+            // Error[2]
+            if (!string.IsNullOrEmpty(msalException.Message))
+            {
+                sqlErs.Add(new SqlError(0, (byte)0x00, (byte)TdsEnums.MIN_ERROR_CLASS,
+                                        connectionOptions.DataSource, msalException.Message,
+                                        ActiveDirectoryAuthentication.MSALGetAccessTokenFunctionName, 0));
+            }
+            return SqlException.CreateException(sqlErs, "", sender);
         }
 
         internal static InvalidOperationException InvalidMixedUsageOfSecureAndClearCredential()
