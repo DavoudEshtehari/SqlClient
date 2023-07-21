158ec039b4c61f27756ac1232884ab5d37168ed1
Davoud <v-deshtehari@microsoft.com>
2023-07-20 17:54:15 -0700

    disable pooling

--- src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlConnectionBasicTests.cs
+++ src/Microsoft.Data.SqlClient/tests/FunctionalTests/SqlConnectionBasicTests.cs
@@ -264,7 +264,7 @@ namespace Microsoft.Data.SqlClient.Tests
         {
             // Start a server with connection timeout from the inline data.
             using TestTdsServer server = TestTdsServer.StartTestServer(false, false, timeout);
-            using SqlConnection connection = new SqlConnection(server.ConnectionString);
+            using SqlConnection connection = new SqlConnection(server.ConnectionString +"; pooling=false;");
 
             // Dispose the server to force connection timeout 
             server.Dispose();
@@ -303,7 +303,7 @@ namespace Microsoft.Data.SqlClient.Tests
         {
             // Start a server with connection timeout from the inline data.
             using TestTdsServer server = TestTdsServer.StartTestServer(false, false, timeout);
-            using SqlConnection connection = new SqlConnection(server.ConnectionString);
+            using SqlConnection connection = new SqlConnection(server.ConnectionString + "; pooling=false;");
 
             // Dispose the server to force connection timeout 
             server.Dispose();
